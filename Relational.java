c82a3ca6b171c5209790d43ff1b700f03a86c7fa
pattya1409 <pprathamesh244@gmail.com>
2026-06-30 16:01:04 +0530

    Update Relational.java

--- Relational.java
+++ Relational.java
@@ -1,10 +1,20 @@
+//Relational operators are used to compare two values or variables. 
+//They always return a boolean value (true or false).
+
+//| Operator | Name                     | Example    | Result  |
+//| -------- | ------------------------ | ---------- | ------- |
+//| `==`     | Equal to                 | `10 == 10` | `true`  |
+//| `!=`     | Not equal to             | `10 != 5`  | `true`  |
+//| `>`      | Greater than             | `10 > 5`   | `true`  |
+////| `<`      | Less than                | `10 < 5`   | `false` |
+//| `>=`     | Greater than or equal to | `10 >= 10` | `true`  |
+//| `<=`     | Less than or equal to    | `10 <= 5`  | `false` |
+
+
 package operators;
 
 public class Relational {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		System.out.println(10 < 20);
 		System.out.println(10 > 5);
 		System.out.println(10 <= 20);
@@ -13,5 +23,13 @@ public class Relational {
 		System.out.println(10 == 6);
 		System.out.println(10 != 10);
 	}
-
 }
+
+//output
+//true
+//true
+//true
+//true
+//true
+//false
+//false
