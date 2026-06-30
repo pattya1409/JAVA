c6ba7130ca576b6f8b7735e8153d1c6b537d6563
pattya1409 <pprathamesh244@gmail.com>
2026-06-30 15:50:31 +0530

    Update variable.java

--- variable.java
+++ variable.java
@@ -1,11 +1,11 @@
+//In Java, a variable is a named memory location used to store data.
+//The value of a variable can change during program execution.
+
 package practice;
 
 public class variable {
-
 	public static void main(String[] args) {
 		var name = "prathamesh";
 		System.out.println(name);
-
 	}
-
 }
