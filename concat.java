69db13f67a6484647fada5bd3b9c4ef1bd4a7c74
pattya1409 <pprathamesh244@gmail.com>
2026-06-30 14:38:19 +0530

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class concat {
+
+	public static void main(String[] args) {
+		var name = "Priya";
+		var message = name + " Will become a software engineer!";
+		System.out.println(message);
+				
+
+	}
+
+}
