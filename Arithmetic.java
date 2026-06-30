99b1e5bb85b68a8618cd3d4cd159dd30bcb9e5e8
pattya1409 <pprathamesh244@gmail.com>
2026-06-30 15:56:44 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,7 +1,9 @@
+//Arithmetic operators are used to perform mathematical operations 
+//such as addition, subtraction, multiplication, division, and finding the remainder.
+
 package operators;
 
 public class Arithmetic {
-
 	public static void main(String[] args) {
 		var a = 10;
 		var b = 5;
@@ -10,7 +12,12 @@ public class Arithmetic {
 		System.out.println(a * b);
 		System.out.println(a / b);
 		System.out.println(a % b);
-
 	}
-
 }
+
+//output 
+//15
+//5
+//50
+//2
+//0
