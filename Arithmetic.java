4f201ed7d4f70278ce7b0d1e3139aebc2244dbc0
pattya1409 <pprathamesh244@gmail.com>
2026-06-30 15:58:20 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,6 +1,15 @@
 //Arithmetic operators are used to perform mathematical operations 
 //such as addition, subtraction, multiplication, division, and finding the remainder.
 
+//| Operator | Name                | Example  | Result |
+//| -------- | ------------------- | -------- | ------ |
+//| `+`      | Addition            | `10 + 5` | `15`   |
+//| `-`      | Subtraction         | `10 - 5` | `5`    |
+//| `*`      | Multiplication      | `10 * 5` | `50`   |
+//| `/`      | Division            | `10 / 5` | `2`    |
+//| `%`      | Modulus (Remainder) | `10 % 3` | `1`    |
+
+
 package operators;
 
 public class Arithmetic {
