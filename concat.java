154e7638a2b427dba9a8b10c53c7fc2c026f2dbb
pattya1409 <pprathamesh244@gmail.com>
2026-06-30 15:52:44 +0530

    Update concat.java

--- concat.java
+++ concat.java
@@ -1,13 +1,12 @@
+//Concatenation means joining two or more values together while printing. 
+//In Java, this is commonly done using the + operator.
+
 package practice;
 
 public class concat {
-
 	public static void main(String[] args) {
 		var name = "Priya";
 		var message = name + " Will become a software engineer!";
 		System.out.println(message);
-				
-
 	}
-
 }
