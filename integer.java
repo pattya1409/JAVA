ad30686d35ea17b2b6cb88b264f755c532967909
pattya1409 <pprathamesh244@gmail.com>
2026-07-01 20:21:21 +0530

    Restore integer class and add output comments
    Restored the integer class structure and output comments.

--- integer.java
+++ integer.java
@@ -1,7 +1,6 @@
 package datatypes;
 
 public class integer {
-
 	public static void main(String[] args) {
 		byte a = 20;
 		short b = 15000;
@@ -12,7 +11,12 @@ public class integer {
 		System.out.println("short: " + b);
 		System.out.println("int: " + c);
 		System.out.println("long: " + d);
-
 	}
-
 }
+
+//output
+//byte: 20
+//short: 15000
+//int: 12345566
+//long: 1020300330031
+
