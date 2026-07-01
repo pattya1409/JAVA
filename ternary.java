ce4cc64ee262fd52a4bcd83ab3a4d4225156b38f
pattya1409 <pprathamesh244@gmail.com>
2026-07-01 20:15:31 +0530

    Add output comments to ternary.java
    Added output comments to clarify the result of the ternary operation.

--- ternary.java
+++ ternary.java
@@ -1,13 +1,13 @@
 package operators;
 
 public class ternary {
-
 	public static void main(String[] args) {
 		var a = 10;
 		var b = 100;
 		var res = (a > b) ? "a is greater than b" : "b is greater than a";
 		System.out.println(res);
-
 	}
-
 }
+
+//output
+//b is greater than a
