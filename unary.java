5baa4b4e45a186a8046c78b2eb9b44a65cbc378b
pattya1409 <pprathamesh244@gmail.com>
2026-07-01 14:37:40 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class unary {
+
+	public static void main(String[] args) {
+	var a = 10;
+	var b = ++a + a++ + --a + a++ + a-- + --a;
+	System.out.println(a);
+	System.out.println(b);
+
+	}
+
+}
