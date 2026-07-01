f265ff4a52bc7f8329f191a0bad536bf22081d27
pattya1409 <pprathamesh244@gmail.com>
2026-07-01 20:20:13 +0530

    Add files via upload

---
+++ integer.java
@@ -0,0 +1,18 @@
+package datatypes;
+
+public class integer {
+
+	public static void main(String[] args) {
+		byte a = 20;
+		short b = 15000;
+		int c = 12345566;
+		long d = 1020300330031l;
+
+		System.out.println("byte: " + a);
+		System.out.println("short: " + b);
+		System.out.println("int: " + c);
+		System.out.println("long: " + d);
+
+	}
+
+}
