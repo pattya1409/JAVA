ed85cf8ce498ae66ad62e144376b98b69db6a762
pattya1409 <pprathamesh244@gmail.com>
2026-07-01 14:37:16 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class Bitwise {
+
+	public static void main(String[] args) {
+		var a = 5;
+		var b = 3;
+		System.out.println(a & b);
+		System.out.println(a | b);
+		System.out.println(a ^ b);
+		System.out.println(~a);
+		System.out.println(a << 2);
+		System.out.println(a >> 2);
+
+	}
+
+}
