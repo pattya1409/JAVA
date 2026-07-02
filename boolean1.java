b62caab68f5c23b7640d754d3f9a9c873359964b
pattya1409 <pprathamesh244@gmail.com>
2026-07-02 16:23:52 +0530

    Add output comments to boolean1.java
    Added output comments to boolean1.java

--- boolean1.java
+++ boolean1.java
@@ -1,12 +1,15 @@
 package datatypes;
 
 public class boolean1 {
-
 	public static void main(String[] args) {
 		boolean a = true;
 		boolean b = false;
 		System.out.println(a);
 		System.out.println(b);
 	}
-
 }
+
+//output
+//true
+//false
+
