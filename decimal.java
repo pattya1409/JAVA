c752cfdd5dfa35e8e673afe43ae93362772108ec
pattya1409 <pprathamesh244@gmail.com>
2026-07-02 16:22:10 +0530

    Add output comments to decimal.java
    Added output comments to decimal.java for clarity.

--- decimal.java
+++ decimal.java
@@ -1,12 +1,14 @@
 package datatypes;
 
 public class decimal {
-
 	public static void main(String[] args) {
 		float a = 14.7f;
 		double b = 12323.49394;
 		System.out.println(a);
 		System.out.println(b);
 	}
-
 }
+
+//output
+//14.7
+//12323.49394
