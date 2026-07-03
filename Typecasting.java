08c6291a36bdccd1b3b844776d8f70f1116e23f2
pattya1409 <pprathamesh244@gmail.com>
2026-07-03 13:04:21 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		int a = 50;
+		double b;
+		b = a;
+		System.out.println(a);
+		System.out.println(b);
+		
+		double c = 123.456;
+		int d;
+		d = (int)c;
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
