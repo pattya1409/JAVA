23065275cabb746a613f3f55d3ce2caef8c51ce5
pattya1409 <pprathamesh244@gmail.com>
2026-07-04 10:27:41 +0530

    Refactor pgm4.java formatting and add output comment

--- pgm4.java
+++ pgm4.java
@@ -1,14 +1,16 @@
 package method;
 
 public class pgm4 {
-
 	public static void main(String[] args) {
 	int c = add(10,20);
 	System.out.println("add two number: " + c);
 	}
+	
 	static int add(int a, int b) {
 		int res = a + b;
 		return res;
 	}
-
 }
+
+//output 
+//add two number: 30
