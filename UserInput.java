c0b8bf37e95cce21377462caec2d0e80c8eeb00a
pattya1409 <pprathamesh244@gmail.com>
2026-07-08 22:39:54 +0530

    Refactor UserInput.java for improved readability

--- UserInput.java
+++ UserInput.java
@@ -3,14 +3,11 @@ package userInput;
 import java.util.Scanner;
 
 public class UserInput {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter name : ");
 		String name = sc.nextLine();
 		System.out.println(name);
-		
-		
 		System.out.println("enter byte");
 		byte a = sc.nextByte();
 		System.out.println(a);
@@ -35,9 +32,34 @@ public class UserInput {
 		System.out.println("enter char");
 		char ch = sc.next().charAt(1);
 		System.out.println(ch);
-		
-		
-
 	}
-
 }
+
+//output
+//enter name : 
+//prathamesh ravindra patil
+//prathamesh ravindra patil
+//enter byte
+//12
+//12
+//enter short
+//130
+//130
+//enter int
+//12000
+//12000
+//enter long
+//1234567
+//1234567
+//enter float
+//13.4
+//13.4
+//enter double
+//234.7
+//234.7
+//enter boolean
+//true
+//true
+//enter char
+//prathamesh
+//r
