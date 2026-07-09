ed8bad2b73728aa8ccb3d69d73641e9c58ccfbfb
pattya1409 <pprathamesh244@gmail.com>
2026-07-09 23:07:09 +0530

    Add output comments to IfElse.java
    Added output comments to IfElse.java for clarity.

--- IfElse.java
+++ IfElse.java
@@ -3,18 +3,19 @@ package conditionStatement;
 import java.util.Scanner;
 
 public class IfElse {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.println("Enter temp: ");
 		int temp = sc.nextInt();
-		
 		if (temp > 20) {
 			System.out.println("its a warm");
 		} else {
 			System.out.println("its a chily");
 		}
 	}
-
 }
+
+//output
+//Enter temp: 
+//21
+//its a warm
