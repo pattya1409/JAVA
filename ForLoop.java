525790ca8e2cbec3b1bc5f12deb65b4285e7290e
pattya1409 <pprathamesh244@gmail.com>
2026-07-10 22:05:45 +0530

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -3,13 +3,10 @@ package ControlFlow;
 import java.util.Scanner;
 
 public class ForLoop {
-
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter a number: ");
-		
 		int num = scan.nextInt();
-		
 		for(int i = 1; i <= num; i++) {
 			if (i % 2 == 0) {
 				System.out.println(i + " is even");
@@ -18,5 +15,18 @@ public class ForLoop {
 			}
 		}
 	}
-
 }
+
+//output
+//Enter a number: 
+//10
+//1 is odd
+//2 is even
+//3 is odd
+//4 is even
+//5 is odd
+//6 is even
+//7 is odd
+//8 is even
+//9 is odd
+//10 is even
