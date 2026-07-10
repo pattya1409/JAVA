b0aad2e42470555f04f92596c3e37f6d3ad817da
pattya1409 <pprathamesh244@gmail.com>
2026-07-10 22:04:04 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,22 @@
+package ControlFlow;
+
+import java.util.Scanner;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		
+		int num = scan.nextInt();
+		
+		for(int i = 1; i <= num; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i + " is even");
+			} else {
+				System.out.println(i + " is odd");
+			}
+		}
+	}
+
+}
