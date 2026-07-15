7a5f354ef81b2270393093f25726c1e1f40a3789
pattya1409 <pprathamesh244@gmail.com>
2026-07-15 22:37:23 +0530

    Add files via upload

---
+++ NestedLoop.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class NestedLoop {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= 5; j++) {
+				System.out.println(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
